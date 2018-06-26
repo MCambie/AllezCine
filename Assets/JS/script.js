54400296788e2200b74240308ff2bdbe7908818d
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-06-26 16:29:42 +0200

    Fin section movies/series

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -94,3 +94,115 @@ $('.launch-modal').on('click', function(e){
     e.preventDefault();
     $( '#' + $(this).data('modal-id') ).modal();
 });
+
+// filtre Boutons films
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btnMovies");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+// filtre bouton SERIES
+filterSelectionSeries("all")
+function filterSelectionSeries(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDivSeries");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+var btnContainer = document.getElementById("myBtnContainerSeries");
+var btns = btnContainer.getElementsByClassName("btnSeries");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+// bouton more/less movies
+function togglediv(bouton, id) {
+var div = document.getElementById(id);
+if(div.style.display=="none") {
+div.style.display = "block";
+bouton.innerHTML = "Moins de films";
+} else {
+div.style.display = "none";
+bouton.innerHTML = "plus de films";
+}
+}
+
+// bouton more/less Series
+function prout(bouton, id) {
+var div = document.getElementById(id);
+if(div.style.display=="none") {
+div.style.display = "block";
+bouton.innerHTML = "Moins de films";
+} else {
+div.style.display = "none";
+bouton.innerHTML = "plus de films";
+}
+}
