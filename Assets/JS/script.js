9fddd30bcccff16da3549fde8b193ea8a751da95
Liedekerke <nicolasdeliedekerke@gmail.com>
2018-06-29 11:28:57 +0200

    lh

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,30 +1,30 @@
 // Entrée sur le site restriction d'âge ->
-// window.onload=function (){
-//     var age = prompt("Please enter your age");
-//
-//     if (age >= 18) {
-//         return;
-//       }
-//     else {
-//       location.href='https://www.imdb.com/';
-// }
-//
-// };
+window.onload=function (){
+    var age = prompt("Please enter your age");
+
+    if (age >= 18) {
+        return;
+      }
+    else {
+      location.href='https://www.imdb.com/';
+}
+
+};
 
 // Bandeau utilisation des cookies
-// $(document).ready(function(){
-//           $('#cookies').animate({
-//              marginTop: "0",
-//          }, 500);
-//
-//          $("#cookies_close").mousedown(function(){
-//
-//                      $('#cookies').animate({
-//                              marginTop: "-35px",
-//                      }, 500);
-//
-//             });
-//   });
+$(document).ready(function(){
+          $('#cookies').animate({
+             marginTop: "0",
+         }, 500);
+
+         $("#cookies_close").mousedown(function(){
+
+                     $('#cookies').animate({
+                             marginTop: "-35px",
+                     }, 500);
+
+            });
+  });
 
 // Modal Login
 $(document).ready(function() {
